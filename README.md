89df837f249e3d5c4eb35ff14259870483da4ff9
Riley <rileyel@haikulearning.com>
2013-09-19 10:07:45 -0500

    Cleanup and tests

--- README.md
+++ README.md
@@ -5,26 +5,47 @@ Easy wizard workflows with no network requests.
 ## Getting Started
 Download the [production version][min] or the [development version][max].
 
-[min]: https://raw.github.com/rileylark/wizard/master/dist/wizard.min.js
-[max]: https://raw.github.com/rileylark/wizard/master/dist/wizard.js
+[min]: https://raw.github.com/haikulearning/wizard/master/dist/wizard.min.js
+[max]: https://raw.github.com/haikulearning/wizard/master/dist/wizard.js
 
 In your web page:
 
 ```html
-<script src="jquery.js"></script>
-<script src="dist/wizard.min.js"></script>
+<div id="wizard1">
+    <div data-wizard-step id="step1">
+        Step 1!
+    </div>
+    <div data-wizard-step id="second-step">
+        Step 2!
+    </div>
+    <div data-wizard-step id="third-step">
+        This is the third step.
+    </div>
+
+    <button data-wizard-next-button id="next-button">
+        Click this to move to the next page (or finish)
+    </button>
+    <button data-wizard-back-button>
+        Click this to move to the previous page
+    </button>
+</div>
+
 <script>
-jQuery(function($) {
-  $.awesome(); // "awesome"
-});
+    jQuery('#wizard1').wizard({
+        initialState : '#step1',
+        mapToNextStep : {
+            '#step1' : '#second-step',
+            '#second-step' : '#third-step'
+        }
+    });
 </script>
 ```
 
 ## Documentation
-_(Coming soon)_
+See src\wizard.js
 
 ## Examples
-_(Coming soon)_
+See the `demo` directory
 
 ## Release History
-_(Nothing yet)_
+2013-9-19: v 0.1.0! Basic navigation through wizard.
--- test/wizard_test.js
+++ test/wizard_test.js
@@ -1,63 +1,67 @@
-(function($) {
-  /*
-    ======== A Handy Little QUnit Reference ========
-    http://api.qunitjs.com/
+( function($) {
+        /*
+         ======== A Handy Little QUnit Reference ========
+         http://api.qunitjs.com/
 
-    Test methods:
-      module(name, {[setup][ ,teardown]})
-      test(name, callback)
-      expect(numberOfAssertions)
-      stop(increment)
-      start(decrement)
-    Test assertions:
-      ok(value, [message])
-      equal(actual, expected, [message])
-      notEqual(actual, expected, [message])
-      deepEqual(actual, expected, [message])
-      notDeepEqual(actual, expected, [message])
-      strictEqual(actual, expected, [message])
-      notStrictEqual(actual, expected, [message])
-      throws(block, [expected], [message])
-  */
+         Test methods:
+         module(name, {[setup][ ,teardown]})
+         test(name, callback)
+         expect(numberOfAssertions)
+         stop(increment)
+         start(decrement)
+         Test assertions:
+         ok(value, [message])
+         equal(actual, expected, [message])
+         notEqual(actual, expected, [message])
+         deepEqual(actual, expected, [message])
+         notDeepEqual(actual, expected, [message])
+         strictEqual(actual, expected, [message])
+         notStrictEqual(actual, expected, [message])
+         throws(block, [expected], [message])
+         */
 
-  module('jQuery#awesome', {
-    // This will run before each test in this module.
-    setup: function() {
-      this.elems = $('#qunit-fixture').children();
-    }
-  });
+        module('jQuery#wizard', {
+            // This will run before each test in this module.
+            setup : function() {
+                this.elems = $('#qunit-fixture').children();
+                this.wizard1 = $('#wizard1').wizard({
+                    initialState : '#step1',
+                    mapToNextStep : {
+                        '#step1' : '#second-step'
+                    }
+                });
+            }
+        });
 
-  test('is chainable', function() {
-    expect(1);
-      strictEqual(1, 1);
-    // Not a bad test to run on collection methods.
-    //strictEqual(this.elems.wizard(), this.elems, 'should be chainable');
-  });
-//
-//  test('is awesome', function() {
-//    expect(1);
-//    strictEqual(this.elems.awesome().text(), 'awesome0awesome1awesome2', 'should be awesome');
-//  });
-//
-//  module('jQuery.awesome');
-//
-//  test('is awesome', function() {
-//    expect(2);
-//    strictEqual($.awesome(), 'awesome.', 'should be awesome');
-//    strictEqual($.awesome({punctuation: '!'}), 'awesome!', 'should be thoroughly awesome');
-//  });
-//
-//  module(':awesome selector', {
-//    // This will run before each test in this module.
-//    setup: function() {
-//      this.elems = $('#qunit-fixture').children();
-//    }
-//  });
-//
-//  test('is awesome', function() {
-//    expect(1);
-//    // Use deepEqual & .get() when comparing jQuery objects.
-//    deepEqual(this.elems.filter(':awesome').get(), this.elems.last().get(), 'knows awesome when it sees it');
-//  });
+        test('is chainable', function() {
+            expect(1);
+            strictEqual(this.elems.wizard({
+                initialState : '#step1',
+                mapToNextStep : {
+                    '#step1' : '#second-step',
+                    '#second-step' : '#third-step'
+                }
+            }), this.elems, 'should be chainable');
+        });
 
-}(jQuery));
+        test('hides all steps but the initial one', function() {
+            expect(2);
+
+            var visibleItems = this.wizard1.find('[data-wizard-step]:visible');
+            strictEqual(1, visibleItems.length, "Exactly one step should be visible.");
+            strictEqual(visibleItems[0], $('#step1')[0], '#step1 should be visible');
+        });
+
+        test('goes from first step to second step', function() {
+            expect(1);
+            
+            stop();
+            $('#next-button').click();
+            var wizard = this.wizard1;
+            setTimeout(function() {
+                strictEqual(wizard.find('[data-wizard-step]:visible')[0], $('#second-step')[0], 'step 2 should be shown');
+                start();
+            }, 450); //need delay to wait for animations to finish
+        });
+
+    }(jQuery));
